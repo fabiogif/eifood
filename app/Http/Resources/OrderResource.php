eaf57a534c7abd3e85f1fe5162689187ffbdb894
fabioinfracommerce <fabio.santos.ext@infracommerce.com.br>
2021-10-11 17:32:14 -0300

    Correções

--- app/Http/Resources/OrderResource.php
+++ app/Http/Resources/OrderResource.php
@@ -2,7 +2,10 @@
 
 namespace App\Http\Resources;
 
+use Carbon\Carbon;
 use Illuminate\Http\Resources\Json\JsonResource;
+use App\Http\Resources\TenantResource;
+use App\Http\Resources\EvaluationResource;
 
 class OrderResource extends JsonResource
 {
@@ -18,9 +21,12 @@ class OrderResource extends JsonResource
             'identify' => $this->identify,
             'total' => $this->total,
             'status' => $this->status,
+            'date' => Carbon::parse($this->created_at)->format('d/m/Y'),
+            'company' => new TenantResource($this->tenant),
             'client' => $this->client_id ? new ClientResource($this->client) : '',
             'table' => $this->table_id ? new TableResource($this->table) : '',
             'products' => ProductResource::collection($this->products),
+            'evaluantion' => EvaluationResource::collection($this->evaluantion),
 
         ];
     }
